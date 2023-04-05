bc77fd48b3edad14f77638362c426a22a88b0dc4
Naty <natyaith@yahoo.com.br>
2023-04-04 22:36:43 -0300

    Add lista suspensa

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,7 +1,19 @@
+import ListaSuspensa from '../ListaSuspensa'
 import TextField from '../TextField/TextField'
 import './Formulario.css'
 
 const Formulario = () => {
+
+    const racas = [
+        'Maltês',
+        'Pug',
+        'Golden',
+        'SRD',
+        'Yorkshire',
+        'Husky',
+        'Buldogue Francês'
+    ]
+
     return (
         <section className='formulario'>
             <form>
@@ -9,6 +21,7 @@ const Formulario = () => {
                 <TextField label="Nome" placeholder="Digite o nome do seu cão"/>
                 <TextField label="Idade" placeholder="Digite a idade"/>
                 <TextField label="Imagem" placeholder="Digite o endereço da foto"/>            
+                <ListaSuspensa label="Raças" itens={racas}/>
             </form>
         </section>
     )
